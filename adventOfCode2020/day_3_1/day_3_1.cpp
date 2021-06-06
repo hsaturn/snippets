fd71721d8487c11232e584698f513aba73f37264
hsaturn <hsaturn.gmail.com>
2021-06-06 13:03:50 +0200

    Day 3 part 1

--- adventOfCode2020/day_3_1/day_3_1.cpp
+++ adventOfCode2020/day_3_1/day_3_1.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
-#include <map>
+#include <string>
 #include <vector>
 
 using namespace std;
 
 int main(int argc, const char* argv[])
 {
-	cout << "Hello world" << endl;
+  vector<string> patterns;
+  while(cin)
+  {
+    string pattern;
+    getline(cin, pattern);
+    if (pattern.length()) patterns.push_back(pattern);
+  }
+  int x=0, y=0, trees=0;
+  const int dx=3, dy=1;
+  while(y<patterns.size())
+  {
+    if (patterns[y][x % patterns[y].length()] == '#') trees++;
+    x+=dx; y+=dy;
+  }
+  cout << trees << endl;
 	return 0;
 }
 
