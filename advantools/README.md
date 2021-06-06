5f0347abd47812a1b3165cc17908f99eaf37c7a6
hsaturn <hsaturn.gmail.com>
2021-06-06 11:37:18 +0200

    Adventofcode adds

---
+++ advantools/README.md
@@ -0,0 +1,1 @@
+# Answers to adventofcode.com
---
+++ advantools/ad_1/ad_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+  while(cin)
+  {
+    int num;
+    cin >> num;
+    cout << "num =" << num << endl;
+  }
+	return 0;
+}
+
+
