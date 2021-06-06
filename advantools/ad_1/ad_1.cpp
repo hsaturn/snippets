87214846502b94eea02e6b45942b250aee80f0b5
hsaturn <hsaturn.gmail.com>
2021-06-06 11:50:59 +0200

    output simplified

--- advantools/ad_1/ad_1.cpp
+++ advantools/ad_1/ad_1.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
-#include <map>
-#include <vector>
+#include <set>
 
 using namespace std;
 
 int main(int argc, const char* argv[])
 {
+  set<int> input;
   while(cin)
   {
     int num;
     cin >> num;
-    cout << "num =" << num << endl;
+    for(const auto &old: input)
+      if (old + num == 2020)
+      {
+        cout << old * num << endl;
+        return 0;
+      }
+    input.insert(num);
   }
-	return 0;
+  cout << "NO ANSWER" << endl;
+	return -1;
 }
 
 
