d032847ce69665d11e2f53269520a9981699b12f
hsaturn <hsaturn@gmail.Com>
2021-07-05 06:53:26 +0200

    Add ansi escape sequences

---
+++ ansi/ansi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include "ansi.h"
+
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+	cout << ansi::clr;
+	cout << "Je " << ansi::savepos << endl;
+	cout << "Interrupted" << endl;
+	cout << ansi::restorepos << "pense !" << endl;
+	cout << ansi::gotoxy(1,1) << "ONE ONE";
+	cout << ansi::gotoxy(10,10) << "TEN TEN" << endl;
+	return 0;
+}
+
+
---
+++ ansi/ansi.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <iostream>
+
+namespace ansi
+{
+	const char* clr="\033[2J";
+	const char* clr_after="\033[J";
+	const char* clr_scrollback="\033[3J";
+
+	const char* savepos="\033[s";
+	const char* restorepos="\033[u";
+
+	const char* black="\033[30m";
+	const char* red="\033[31m";
+	const char* green="\033[32m";
+	const char* yellow="\033[33m";
+	const char* blue="\033[34m";
+	const char* magenta="\033[35m";
+	const char* cyan="\033[36m";
+	const char* white="\033[37m";
+	
+	const char* bg_black="\033[40m";
+	const char* bg_red="\033[41m";
+	const char* bg_green="\033[42m";
+	const char* bg_yellow="\033[43m";
+	const char* bg_blue="\033[44m";
+	const char* bg_magenta="\033[45m";
+	const char* bg_cyan="\033[46m";
+	const char* bg_white="\033[47m";
+
+	const char* reset="\033c";
+
+	class gotoxy
+	{
+		public:
+			gotoxy(int row, int col) : row(row), col(col){}
+			std::ostream& operator()(std::ostream& out) const
+			{
+				return out << "\033[" << row << ';' << col << 'H';
+			}
+		private:
+			int row,col;
+	};
+
+	std::ostream& operator<<(std::ostream& out, const gotoxy& go)
+	{
+		return go(out);
+	}
+};
